948619050202cab133e92117445ad308ed04f794
Nyholm <tobias.nyholm@gmail.com>
2013-06-22 16:57:11 +0200

    did minor changes

--- src/Happyr/ApiClient/Configuration.php
+++ src/Happyr/ApiClient/Configuration.php
@@ -5,25 +5,25 @@ namespace Happyr\ApiClient;
 
 class Configuration
 {
-	public $username='';//your username here
-	public $token=''; //Your api token goes here
-	public $baseUrl='http://happyrecruiting.se/api/'; //must end with a slash (/)
-	public $version='1.0';
-	public $format='xml'; //You should have a good reason not to choose xml.
-	
-	public $enableExceptions=false;//if true, we will throw exceptions on error
-	public $debug=false; //enables debug mode
+    public $username='';//your username here
+    public $token=''; //Your api token goes here
+    public $baseUrl='http://happyrecruiting.se/api/'; //must end with a slash (/)
+    public $version='1.0';
+    public $format='xml'; //You should have a good reason not to choose xml.
 
-	public function __construct($username=null,$token=null)
-	{
-		if($token!=null){
-			$this->token=$token;
-		}	
-		
-		if($username!=null){
-			$this->username=$username;
-		}	
-	}
-		
-	
+    public $enableExceptions=false;//if true, we will throw exceptions on error
+    public $debug=false; //enables debug mode
+
+    public function __construct($username=null,$token=null)
+    {
+        if($token!=null){
+            $this->token=$token;
+        }
+
+        if($username!=null){
+            $this->username=$username;
+        }
+    }
+
+
 }
