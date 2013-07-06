ce6640bb0f7e7dddd6c205ba1c5b957395affe6f
Nyholm <tobias.nyholm@gmail.com>
2013-07-06 13:26:17 +0200

    Minor

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of Ekino New Relic bundle.
- *
- * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
     include_once $file;
